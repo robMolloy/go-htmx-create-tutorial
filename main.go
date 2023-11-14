b76fbc6688e88281c8ee7455f3ffe2a3568c0989
robmolloyntt <robert.molloy@nttdata.com>
2023-11-14 11:05:46 +0000

    lesson-2-setting-up-your-go-server

--- main.go
+++ main.go
@@ -6,17 +6,14 @@ import (
 	"net/http"
 
 	chi "github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 	cors "github.com/go-chi/cors"
 )
-
-const url = "localhost:3002"
 
 func main() {
 	fmt.Println(`Server started`)
 	r := chi.NewRouter()
-
-	// Basic CORS
-	// for more ideas, see: https://developer.github.com/v3/#cross-origin-resource-sharing
+	r.Use(chiMiddleware.Logger)
 	r.Use(cors.Handler(cors.Options{
 		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
 		AllowedOrigins: []string{"https://*", "http://*"},
@@ -31,8 +28,26 @@ func main() {
 	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome"))
 	})
-	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+	r.Get("/foo1", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("welcome from foo"))
+	})
+	r.Get("/html", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+ hello world
+</body>
+</html>`))
+	})
+
+	r.Get("/add-contact", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte(`<li class="p-1 mb-1 bg-blue-100 rounded-xl">server - server</li>`))
 	})
 
 	log.Fatal(http.ListenAndServe("localhost:3002", r))
